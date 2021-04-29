079e18cce6a31c7efbb1aaa7a4aa5a151acc4384
Ruan <ruangbarros@gmail.com>
2021-04-28 21:41:00 -0300

    Add project files.

---
+++ MyClasses/FileProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace MyClasses
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (!string.IsNullOrEmpty(fileName))
+                return File.Exists(fileName);
+            else
+                throw new ArgumentNullException("fileName");
+        }
+    }
+}
