60707489450fe895691641967a768f0ac22afd25
Ruan <ruangbarros@gmail.com>
2021-05-10 21:58:03 -0300

    creating and using App.config

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -1,19 +1,37 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClasses;
 using System;
+using System.Configuration;
+using System.IO;
 
 namespace MyClassesTest
 {
     [TestClass]
     public class FileProcessTest
     {
+        private string _GoodFileName;
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["_GoodFileName"];
+
+            if (_GoodFileName.Contains("[AppPath]"))
+                _GoodFileName = _GoodFileName.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+        }
+
         [TestMethod]
         public void FileNameDoesExists()
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
 
-            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            SetGoodFileName();
+
+            File.AppendAllText(_GoodFileName, "Some Text");
+
+            fromCall = fp.FileExists(_GoodFileName);
+
+            File.Delete(_GoodFileName);
 
             Assert.IsTrue(fromCall);
         }
