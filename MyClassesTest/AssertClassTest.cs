84cdc54bef01fcea1927cd8c6cab9f69f7caa795
Ruan <ruangbarros@gmail.com>
2021-05-19 20:36:54 -0300

    create AssertClassTest

---
+++ MyClassesTest/AssertClassTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    class AssertClassTest
+    {
+        #region AreEqual/AreNotEqual Tests
+
+        [TestMethod]
+        [Owner("Ruan")]
+        public void AreEqualTest()
+        {
+            string str1 = "Rodrigo";
+            string str2 = "rodrigo";
+
+            Assert.AreEqual(str1, str2);
+        }
+
+        [TestMethod]
+        [Owner("Ruan")]
+        [ExpectedException(typeof(AssertFailedException))]
+        public void AreEqualCaseSensitiveTest()
+        {
+            string str1 = "Rodrigo";
+            string str2 = "rodrigo";
+
+            Assert.AreEqual(str1, str2, false);
+        }
+
+        #endregion
+    }
+}
